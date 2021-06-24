4bf2af5e9eedf5f80c130e3ce122cce48569c1b1
andrewlsteele <asteele9123@gmail.com>
2021-06-24 12:14:14 +0100

    Experimented with p5, creating a grid

--- Main.js
+++ Main.js
@@ -1,1 +1,24 @@
 console.log("Hello world");
+
+let canvasWidth = 900;
+let canvasHeight = 900;
+let boxWidth = 9;
+
+function setup() {
+
+    createCanvas(canvasWidth, canvasHeight);
+    background(220);
+}
+
+function draw() {
+
+    stroke(160);
+
+    for (let i=0; i<canvasWidth/boxWidth; i++) {
+        line(i*boxWidth, canvasHeight, i*boxWidth, 0);
+    }
+
+    for (let i=0; i<canvasHeight/boxWidth; i++) {
+        line(canvasWidth, i*boxWidth, 0, i*boxWidth);
+    }
+}
